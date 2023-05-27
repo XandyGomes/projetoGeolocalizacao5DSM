c5dd34ec464de89ad1b1804fc6c7ce118621af5c
XandyGomes <x3.computadores@hotmail.com>
2023-05-27 08:08:20 -0300

    Aula.27.05.2023

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+import { requestMultiple, PERMISSIONS } from 'react-native-permissions';
+import Geolocation from 'react-native-geolocation-service';
+import { Platform } from 'react-native';
+
+export default () => {
+
+}
---
+++ src/pages/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+export default class Main extends Component {
+
+  render() {
+    return (
+      <Container>
+        <Text>Teste</Text>
+      </Container>
+    );
+  }
+}
